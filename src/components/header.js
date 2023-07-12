1d72ab57deb9091506c11f5d64a7a2a5a1acd352
Kaleb Alves <kaleblucasalves@hotmail.com>
2023-07-12 20:44:33 -0300

    added: styled component no header

--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,52 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const HeaderContainer = styled.header`
+	background-color: #141414;
+	padding: 20px; 
+`;
+
+const Title = styled.h1`
+	font-size: 24px;
+	color: #bbbbbb;
+`;
+
+const Nav = styled.nav`
+	ul {
+		list-style: none;
+		display: flex;
+	
+
+	li {
+		margin-right: 15px;
+	
+
+		a {	
+		text-decoration: none;
+		color: #bbbbbb;
+		font-weight: bold;
+
+			&:hover{
+			color:#0f1653;
+			}
+		}
+	}
+}
+`;
 
 const Header = () => {
     return(
-        <header>
-            <h1>Meu Portfolio</h1>
-            <nav>
+        <HeaderContainer>
+            <Title>Meu Portfolio</Title>
+            <Nav>
                 <ul>
                     <li><a href="#about">Sobre</a></li>
                     <li><a href="#skills">Habilidades</a></li>
                     <li><a href="#projects">Projetos</a></li>
                     <li><a href="#contact">Contato</a></li>
                 </ul>
-            </nav>
-        </header>
+            </Nav>
+        </HeaderContainer>
     );
 };
 
