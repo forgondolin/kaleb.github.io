0fed9673498af70669dff1dd53b942a4f8167c32
Kaleb Alves <kaleblucasalves@hotmail.com>
2023-07-20 19:13:08 -0300

    melhorando o estilo da pagina

--- src/components/carrossel.js
+++ src/components/carrossel.js
@@ -1,8 +1,41 @@
 import React, { useState, useEffect } from 'react';
 import Slider from 'react-slick';
 import axios from 'axios';
+import { styled } from '@mui/system';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+
+const SectionContainer = styled('section')`
+  margin-top: 20px;
+`;
+
+const Title = styled(Typography)`
+  color: #3f51b5;
+  font-size: 24px;
+  margin-bottom: 20px;
+`;
+
+const ProjectContainer = styled('div')`
+  background-color: #f5f5f5;
+  border-radius: 4px;
+  padding: 20px;
+  height: 100%;
+`;
+
+const ProjectTitle = styled(Typography)`
+  color: #333;
+  font-size: 18px;
+  font-weight: bold;
+  margin-bottom: 10px;
+`;
+
+const ProjectDescription = styled(Typography)`
+  color: #666;
+  font-size: 14px;
+  margin-bottom: 15px;
+`;
 
 const Carrossel = () => {
   const [projects, setProjects] = useState([]);
@@ -29,20 +62,20 @@ const Carrossel = () => {
   };
 
   return (
-    <div>
-      <h2>Meus Projetos</h2>
+    <SectionContainer id="projects">
+      <Title variant="h3">Meus Projetos</Title>
       <Slider {...settings}>
         {projects.map((project) => (
-          <div key={project.id}>
-            <h3>{project.name}</h3>
-            <p>{project.description}</p>
-            <a href={project.html_url} target="_blank" rel="noopener noreferrer">
+          <ProjectContainer key={project.id}>
+            <ProjectTitle variant="h6">{project.name}</ProjectTitle>
+            <ProjectDescription variant="body1">{project.description}</ProjectDescription>
+            <Link href={project.html_url} target="_blank" rel="noopener noreferrer">
               Ver no GitHub
-            </a>
-          </div>
+            </Link>
+          </ProjectContainer>
         ))}
       </Slider>
-    </div>
+    </SectionContainer>
   );
 };
 
--- src/components/skills.js
+++ src/components/skills.js
@@ -1,28 +1,83 @@
 import React from 'react';
+import { styled } from '@mui/system';
+import Typography from '@mui/material/Typography';
+import LinearProgress from '@mui/material/LinearProgress';
+
+const SectionContainer = styled('section')`
+  margin-top: 20px;
+`;
+
+const Title = styled(Typography)`
+  color: #3f51b5;
+  font-size: 24px;
+  margin-bottom: 10px;
+`;
+
+const SkillBarContainer = styled('div')`
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+`;
+
+const SkillName = styled(Typography)`
+  flex: 1;
+  color: #333;
+  font-size: 18px;
+`;
+
+const ProgressBarContainer = styled('div')`
+  flex: 3;
+  margin-left: 10px;
+`;
+
+const ProgressLabel = styled(Typography)`
+  color: #666;
+  font-size: 12px;
+  margin-bottom: 5px;
+`;
 
 const Skills = () => {
-    const skills = [
-        'HTML', // 0
-        'CSS',  //1
-        'JS',  //2
-        'React', //3
-        'Docker',
-        'CI/CD'
-    ];
-    
-    return (
-        <section id="skills">
-            <h2>Habilidades</h2>
-            <ul>
-                {skills.map((skill, index) => (
-                    <li key={index}>{skill}</li>
-                ))}
-            </ul>
-        </section>
-    );
+  const skills = [
+    { name: 'HTML', level: 8 },
+    { name: 'CSS', level: 7 },
+    { name: 'JS', level: 6 },
+    { name: 'React', level: 5 },
+    { name: 'Docker', level: 4 },
+    { name: 'CI/CD', level: 3 },
+  ];
+
+  return (
+    <SectionContainer id="skills">
+      <Title variant="h2">Habilidades</Title>
+      {skills.map((skill) => (
+        <SkillBar key={skill.name} name={skill.name} level={skill.level} />
+      ))}
+    </SectionContainer>
+  );
+};
+
+const SkillBar = ({ name, level }) => {
+  return (
+    <SkillBarContainer>
+      <SkillName variant="h4">{name}</SkillName>
+      <ProgressBarContainer>
+        <ProgressLabel>{level}</ProgressLabel>
+        <LinearProgress
+          variant="determinate"
+          value={level * 10}
+          style={{
+            backgroundColor: '#e0e0e0',
+            borderRadius: '4px',
+            height: '8px',
+            '& .MuiLinearProgress-bar': {
+              backgroundColor: '#3f51b5',
+              borderRadius: '4px',
+            },
+          }}
+        />
+      </ProgressBarContainer>
+    </SkillBarContainer>
+  );
 };
 
 export default Skills;
-
-
-
